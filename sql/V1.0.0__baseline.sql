6e0c1b420fd578386a02d767015e6f98e1c2fcb5
Muhammad Fayzan Siddiqui <fayzan.siddiqui@11street.my>
2018-03-21 03:15:39 +0800

    initial

---
+++ sql/V1.0.0__baseline.sql
@@ -0,0 +1,38 @@
+--
+-- Database: `author-admin-db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` int(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `last_login` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
