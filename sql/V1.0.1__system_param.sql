3d2c0cfbd1f8bd412cd4979fa56d688abc1961cd
Muhammad Fayzan Siddiqui <fayzan.siddiqui@11street.my>
2018-04-03 15:28:06 +0800

    fix data type

--- sql/V1.0.1__system_param.sql
+++ sql/V1.0.1__system_param.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `system_param` ( `id` INT NOT NULL AUTO_INCREMENT ,  `name` VARCHAR(50) NOT NULL ,  `value` VARCHAR(50) NOT NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;
+CREATE TABLE `system_param` ( `id` INT NOT NULL AUTO_INCREMENT ,  `name` VARCHAR(50) NOT NULL ,  `value` VARCHAR(100) NOT NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;
--- sql/V1.0.4__add_updated_date_in_system_param.sql
+++ sql/V1.0.4__add_updated_date_in_system_param.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `system_param` ADD `description` DATETIME NOT NULL AFTER `value`;
+ALTER TABLE `system_param` ADD `description` VARCHAR(100) NOT NULL AFTER `value`;
 ALTER TABLE `system_param` ADD `updated_date` DATETIME NOT NULL AFTER `description`;
 ALTER TABLE `system_param` CHANGE `updated_date` `updated_date` DATETIME NULL;
